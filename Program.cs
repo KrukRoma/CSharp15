8e09c64aae8903620050dff2208f8d4dbfe0804f
Roman <Roman@DESKTOP-GE7UVHJ>
2024-07-10 01:14:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file path: ");
+            string filePath = Console.ReadLine();
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    string fileContent = File.ReadAllText(filePath);
+                    Console.WriteLine("File content: ");
+                    Console.WriteLine(fileContent);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist.");
+            }
+        }
+    }
+}
